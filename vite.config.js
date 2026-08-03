7885f6e71de1696322b38550d15618a60351b8c6
Sanjay Boricha <sanjayboricha1001@gmail.com>
2026-08-03 11:14:42 +0530

    Fix gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
+const base = process.env.VITE_BASE_PATH || '/';
+
 export default defineConfig({
+  base,
   plugins: [vue()],
   server: {
     proxy: {
